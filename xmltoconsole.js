4ff8366d28650547d655c9d4c1df91d4c6da57e5
Irvy <Irving.Derin@gmail.com>
2012-12-30 21:40:43 -0500

    Redid it using xml2json, much nicer output! Ready to be socketted out of here and on to a sitegit add . :D

--- xmltoconsole.js
+++ xmltoconsole.js
@@ -1,42 +1,50 @@
-var util = require('util')
-var xml = require('node-xml')
+var parser = require('xml2json'),
+    fs = require('fs'),
+    util = require('util');
 
-var parser = new xml.SaxParser(function(foo){
+fs.readFile(__dirname + '/shows.xml', function(err, data){
+    var json = parser.toJson(data, {object : true});
+    var doc = json["ShowSchedule"]["Film"];
 
-    foo.onStartDocument(function(){
-        console.log("We are now going to be looking into the xml document!")
+    for(var j = 0; j < doc.length; j++){
+         
+        console.log("Title is : " + doc[j]["Title"] );
+        console.log("The rating is : " + doc[j]["Rating"]);
+        console.log("The show times are the following : ") 
+        var shows = doc[j]["Show"]
+        for(var i = 0; i < shows.length; i++){
+            console.log("\n==========================");
+            console.log("Time: " + shows[i]["DateTime"]) 
+            console.log("In Theater " + shows[i]["Aud"]);
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var fs = require('fs'),
+    xml2js = require('xml2js'),
+    util = require('util');
+
+var parser = new xml2js.Parser();
+
+fs.readFile(__dirname + '/shows.xml', function(err, data) {
+    parser.parseString(data, function (err, result){
+        console.log(util.inspect(result, false, null));
+        console.log('Done');
     });
-
-    foo.onEndDocument(function(){
-        console.log("We are done reading through the file!")
-    });
-
-    foo.onStartElementNS(function(element, attrs, prefix, uri, namespaces){
-        console.log("=> Started: " + element );
-        console.log(JSON.stringify(attrs));
-    });
-
-    foo.onEndElementNS(function(element, prefix, uri){
-        util.log("<= End: " + element + "\n");
-        parser.pause();
-        setTimeout(function(){
-            parser.resume();}, 1000)
-    });
-
-})
+});
+*/
 
 
 
-parser.parseString("<html><body>");
-parser.parseString("<!-- This is the start");
-parser.parseString(" and the end of a comment -->");
-parser.parseString("and lots");
-parser.parseString("and lots of text&am");
-parser.parseString("p;some more.");
-parser.parseString("<![CD");
-parser.parseString("ATA[ this is");
-parser.parseString(" cdata ]]>");
-parser.parseString("</body");
-parser.parseString("></html>");
-
-parser.parseFile("shows.xml")
